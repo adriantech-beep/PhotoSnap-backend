a636591e5ed413c3ed1e22ca4db23b0fea9e1fe2
adriantech-beep <Adhrianne29@gmail.com>
2025-10-28 21:46:22 +0800

    replaced Allowed origin url

--- src/server.ts
+++ src/server.ts
@@ -15,9 +15,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5176",
-  "https://photo-snap-e2xx.vercel.app",
-  "https://photo-snap-e2xx-git-main-adriantech-beeps-projects.vercel.app",
-  "https://photo-snap-e2xx-668otdvdt-adriantech-beeps-projects.vercel.app",
+  "https://photo-snap-phi.vercel.app/",
 ];
 
 app.use(
