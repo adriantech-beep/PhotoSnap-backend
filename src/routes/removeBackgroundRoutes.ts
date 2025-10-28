1693abba2973cffa137ac99f43dc4a53b371ef2a
adriantech-beep <Adhrianne29@gmail.com>
2025-10-28 21:22:24 +0800

    Deleted Unnecessary files

--- src/routes/removeBackgroundRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { removeBackgroundLocal } from "../controllers/removeBackgroundController";
-
-const router = express.Router();
-
-router.post("/remove-background", removeBackgroundLocal);
-
-export default router;
